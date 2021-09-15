1a4cf455ef2dfd2279a00d4c70224faac7dd3108
Kirill <pavlov140700@mail.ru>
2021-09-15 15:45:40 +0300

    Initial Commit

---
+++ text_to_speech/text_to_speechApp.swift
@@ -0,0 +1,17 @@
+//
+//  text_to_speechApp.swift
+//  text_to_speech
+//
+//  Created by Кирилл on 15.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct text_to_speechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ text_to_speechTests/text_to_speechTests.swift
@@ -0,0 +1,33 @@
+//
+//  text_to_speechTests.swift
+//  text_to_speechTests
+//
+//  Created by Кирилл on 15.09.2021.
+//
+
+import XCTest
+@testable import text_to_speech
+
+class text_to_speechTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
